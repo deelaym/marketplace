5a35c43651509e205908f909e2d2cd90088f4a39
deelaym <var-mak@mail.ru>
2024-08-29 18:12:40 +0300

    Docker added

--- rabbitmq/config.py
+++ rabbitmq/config.py
@@ -1,13 +1,22 @@
+import os
+
 import aio_pika
 from aio_pika.abc import AbstractRobustConnection, AbstractRobustChannel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 NEW_ORDERS_QUEUE = 'new_orders_queue'
 PROCESSING_ORDERS_QUEUE = 'processing_orders_queue'
 SENDING_NOTIFICATIONS_QUEUE = 'sending_notifications_queue'
 
+RMQ_HOST = os.getenv('RMQ_HOST')
+RMQ_USER = os.getenv('RMQ_USER')
+RMQ_PASS = os.getenv('RMQ_PASS')
+
 
 async def get_connection() -> AbstractRobustConnection:
-    connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
+    connection = await aio_pika.connect_robust(f"amqp://{RMQ_USER}:{RMQ_PASS}@{RMQ_HOST}/")
     return connection
 
 
